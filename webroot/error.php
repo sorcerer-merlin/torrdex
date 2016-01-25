954d63f7588d349de8087c6cc2d0a0b110acb225
sorcerer-merlin <jman.rice.cakes@gmail.com>
2016-01-25 22:01:59 +0300

    Fixed image display on error page when error path is nested directory.

--- webroot/error.php
+++ webroot/error.php
@@ -4,7 +4,7 @@
 ?>
 
 <br>
-<img src="img/minions.png" width="484" height="420" ALT="Oooops?!" />
+<img src="/img/minions.png" width="484" height="420" ALT="Oooops?!" />
 <h1>Oooops?!</h1>
 <h3>The page you are looking for doesn't exist, or may have been removed. Sorry for the inconvenience!</h3>
 <br>
